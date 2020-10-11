3173c7b5c8342c239fef3ace70dfa85363eee1bd
tk0nik <72498532+tk0nik@users.noreply.github.com>
2020-10-11 17:00:41 +0300

    Add files via upload

---
+++ checking_queries_function.py
@@ -0,0 +1,31 @@
+# ==== functions ==== #
+
+def check_query(query):
+    if query.split(): #проверка списка на пустоту
+        if query.split() [0] == 'Анфиса' or query.split() [0] == 'Анфиса,':
+            return 'запрос к Анфисе'
+        else:
+            return 'запрос к кому-то ещё'  
+    else:
+        return 'запрос пуст или некорректен'     
+
+def print_query_info(q):
+    for q in queries:
+        if isinstance(q, str):
+            result = check_query(q)
+            print(q, '-', result)
+        else:
+            print('запрос пуст или некорректен')
+
+# ==== data ==== #
+queries = [
+    'Анфиса, сколько у меня друзей?',
+    'Андрей, ну где ты был?',
+    'Андрей, ну обними меня скорей!',
+    'Анфиса, кто все мои друзья?' #,
+#    '  ', 123, ['ddd', 'dd1']
+]
+            
+# ==== main ==== #
+            
+print_query_info(queries)
