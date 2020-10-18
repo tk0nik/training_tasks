c1fc5f8a32d58eb3a0fb757d30544ee63cebfade
tk0nik <72498532+tk0nik@users.noreply.github.com>
2020-10-18 18:05:57 +0300

    Update requests_module.py

--- requests_module.py
+++ requests_module.py
@@ -1,16 +1,18 @@
 import requests
 
-
+# ==== data ==== #
 url = 'https://wttr.in'  # не изменяйте значение URL
 
 weather_parameters = {
-    '0': '',
-    'T': '',
-    'M': '',
-    'lang': 'ru'
-    # добавьте параметр запроса `T`, чтобы вернулся чёрно-белый текст
+    '0': '', # только текущая погода
+    'T': '', # отключить терминальные последовательности (без цветов)
+    'M': '', # показывать скорость ветра в м/с
 }
 
-response = requests.get(url, params = weather_parameters)  # передайте параметры в http-запрос
+request_headers = {
+    'Accept-Language': 'ru'
+}
 
+# ==== main ==== #
+response = requests.get(url, params = weather_parameters, headers=request_headers)  
 print(response.text)
