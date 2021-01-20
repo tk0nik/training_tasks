cc423f2f4395178c31307794efbbeb8185075518
tk0nik <72498532+tk0nik@users.noreply.github.com>
2021-01-20 21:52:33 +0300

    Update noosphere_hack.py
    Получаю количество страниц из описания комикса на сайте. Скачиваю все разом. Мб, потом допишу ввод диапазона с клавиатуры и кастомные ссылки, чтобы работало и с другими комиксами. Надеюсь, владельцы акомикса судиться не придут.

--- noosphere_hack.py
+++ noosphere_hack.py
@@ -9,6 +9,16 @@ catalog_path = "noosphere"
 
 #====functions====#
 
+def get_count_of_pages():
+    link_about = link_without_number + "about"
+    #print(link_about)
+    #костыли подъехали
+    p_list = str(urllib.request.urlopen(link_about).read()).split("about-summary")[1].split("<p>")  #ага, сплит по тегу <p>
+    result = p_list[4].split("</p>")[0].split("</b>")[1].strip()
+    
+    return int(result)
+
+
 def get_image_link(number):
     link = link_without_number + number
     page = urllib.request.urlopen(link).read()
@@ -16,9 +26,12 @@ def get_image_link(number):
     part = page_txt.split("mainImage")[1].split('"')
     image_link = part[2]
     full_image_link = "https://acomics.ru"+image_link
+    
     return full_image_link
 
-for i in range(1, 229):
+#====main====#
+
+for i in range(1, get_count_of_pages() + 1):
  
     #print(get_image_link(str(i)))
     p = requests.get(get_image_link(str(i)))
@@ -28,6 +41,8 @@ for i in range(1, 229):
     out.write(p.content)
     out.close()
     print("page", i, "is in<"+ catalog_path+"> path")
+    
+print("all pages downloaded!") 
 
 
    
