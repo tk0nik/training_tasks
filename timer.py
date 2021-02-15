67414c1c0c0b18636fb2cad93211aaf633c3ae2f
tk0nik <72498532+tk0nik@users.noreply.github.com>
2021-02-15 12:24:31 +0300

    Version 0 :)
    Новый функционал: выводит оповещения о начале и конце занятий и перемен. 
    В консоли костыльно, но работает. Дальнейший план - сделать это ботом в дискорде

--- timer.py
+++ timer.py
@@ -45,7 +45,6 @@ def time_now():
 
 #убрать потом это из функций (будет юзаться только при включении)
 def weekday_now():
-    #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     today = datetime.datetime.today()
     return weekdays[today.weekday()]
 
@@ -65,12 +64,44 @@ def schedule_today():
 groups_today = schedule_today()
 weekday = weekday_now()
 
+#если сегодня вообще есть занятия
 if groups_today != []:
     print(groups_today)
-    
-print(time_now())
 
+#вывод всего расписания на день
 for i in groups_today:
     group = groups[i]
-    print(group[weekday])
+    print(i+" группа ", group[weekday])
 
+while True:
+    hours = int(time_now()[0:2])
+    minutes = int(time_now()[3:5])
+  
+    for i in groups_today:
+        group = groups[i]
+        class_hours = int(group[weekday][0:2])
+        print(class_hours)
+        
+        while hours < class_hours:
+            print("для занятия группы", i, "ещё рано")
+            time.sleep(3)
+            
+        #TO DO: добавить оповещение за 10 минут
+            
+        if hours == class_hours and minutes ==0:
+            print(time_now())
+            print("О! Начало занятия группы", i)
+            time.sleep(60*30)   #30 минут
+            
+            print("О! Начало перемены у группы", i)
+            #TO DO: добавить таймер обратного отсчёта
+            time.sleep(60*10)   #10 минут
+            
+            print("О! Конец перемены у группы", i)
+            time.sleep(60*30)   #30 минут
+            #TO DO: добавить проверку, что ничего не сбилось
+            print("О! Конец занятия у группы", i)
+            
+    print("Конец рабочего дня!")
+    break
+print("Конец работы программы :)")
