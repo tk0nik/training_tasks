401eaa84918c26825714c99eec01ef8c960ecd92
tk0nik <72498532+tk0nik@users.noreply.github.com>
2021-02-20 11:28:09 +0300

    version 1.0 (с багами)
    Добавлено взаимодействие с сервером discord

--- timer.py
+++ timer.py
@@ -1,9 +1,12 @@
-import datetime
+import discord
+from discord.ext import commands
 import time
+import datetime
 
+#link for add to server: https://discord.com/api/oauth2/authorize?client_id=811552622951137290&permissions=1140980816&scope=bot
+token = 'ODExNTUyNjIyOTUxMTM3Mjkw.YCz3Rw.qICg8ZRt8I6Dzlt1R33Q2Tp8b6Y'
 
-#время начала занятий
-
+#===============DATA================#
 groups = {
     "1": {"Среда":"09:00",
           "Суббота":"09:00"},
@@ -18,17 +21,6 @@ groups = {
           "Суббота":"14:30"},
     }
 
-#лучше вместо этого потом использовать dict keys
-weekdays = [
-        "Понедельник",
-        "Вторник",
-        "Среда",
-        "Четверг",
-        "Пятница",
-        "Суббота",
-        "Воскресенье"
-        ]
-
 schedule = {
         "Понедельник": ["5", "4"],
         "Вторник": [],
@@ -39,69 +31,68 @@ schedule = {
         "Воскресенье": []
     }
 
+weekdays = list(schedule.keys())
+
 def time_now():
     time = datetime.datetime.now().strftime("%H:%M")
     return time
-
-#убрать потом это из функций (будет юзаться только при включении)
+        
 def weekday_now():
     today = datetime.datetime.today()
     return weekdays[today.weekday()]
-
-
-if groups["1"] == time_now():
-    print("начало занятия у группы 1")
-
 
 def schedule_today():
     for day in weekdays:
         if day == weekday_now():
             print("сегодня", weekday_now())
-            
             return schedule[day]
-
-#==== main ====#
+        
+#=================== main ========================#
 groups_today = schedule_today()
 weekday = weekday_now()
 
-#если сегодня вообще есть занятия
-if groups_today != []:
-    print(groups_today)
+bot = commands.Bot(command_prefix="$")
 
-#вывод всего расписания на день
-for i in groups_today:
-    group = groups[i]
-    print(i+" группа ", group[weekday])
-
-while True:
-    hours = int(time_now()[0:2])
-    minutes = int(time_now()[3:5])
-  
-    for i in groups_today:
-        group = groups[i]
-        class_hours = int(group[weekday][0:2])
-        print(class_hours)
+@bot.event
+async def on_ready():
+    print("Logged in as")
+    print(bot.user.name)
+    print("------")
+    guild = bot.guilds[0]
+    category = guild.categories[0] # выбирает первую категорию из сервера, к которому подключился
+    while True:
+        hours = int(time_now()[0:2])
+        minutes = int(time_now()[3:5])
         
-        while hours < class_hours:
-            print("для занятия группы", i, "ещё рано")
-            time.sleep(3)
-            
-        #TO DO: добавить оповещение за 10 минут
-            
-        if hours == class_hours and minutes ==0:
-            print(time_now())
-            print("О! Начало занятия группы", i)
-            time.sleep(60*30)   #30 минут
-            
-            print("О! Начало перемены у группы", i)
-            #TO DO: добавить таймер обратного отсчёта
-            time.sleep(60*10)   #10 минут
-            
-            print("О! Конец перемены у группы", i)
-            time.sleep(60*30)   #30 минут
-            #TO DO: добавить проверку, что ничего не сбилось
-            print("О! Конец занятия у группы", i)
-            
-    print("Конец рабочего дня!")
-    break
-print("Конец работы программы :)")
+        j = 0 #int(input('::'))
+        channel = category.channels[j] # получает первый канал в первой категории
+        #text = input(': ')
+        for i in groups_today:
+            group = groups[i]
+            class_hours = int(group[weekday][0:2])
+            class_minutes = int(group[weekday][3:5])
+            print(class_hours, class_minutes)
+
+            #!!!!!!!! БАГ: НЕ РАБОТАЕТ С МИНУТАМИ! Н-р, 16:20! 
+            while hours <= class_hours:
+                text = "для занятия группы"+str(i)+ "ещё рано"
+                time.sleep(10)
+                await channel.send(text) # отправка самого сообщения
+
+            if hours == class_hours and minutes ==0:
+                text = str(time_now())
+                print("О! Начало занятия группы", i)
+                await channel.send(text)
+                time.sleep(60*30)   #30 минут
+                
+                print("О! Начало перемены у группы", i)
+                #TO DO: добавить таймер обратного отсчёта
+                time.sleep(60*10)   #10 минут
+                
+                print("О! Конец перемены у группы", i)
+                time.sleep(60*30)   #30 минут
+                #TO DO: добавить проверку, что ничего не сбилось
+                print("О! Конец занятия у группы", i)
+        print("Конец рабочего дня!")
+        break
+bot.run(token)
